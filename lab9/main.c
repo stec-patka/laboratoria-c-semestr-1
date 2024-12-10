b11222527e0500625ba2598955d68394ca5d1bcb
stec-patka <stecpatrycja.25@gmail.com>
2024-12-10 22:02:05 +0100

    tyla

--- lab9/main.c
+++ lab9/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-///zad 1
+//zad 1
 void wczytaj(int *a, int *b, int *c)
 {
     printf("podaj trzy liczby");
@@ -32,29 +32,78 @@ void drukuj(const int *a, const int *b, const int *c)
 }
 
 
-///zad2
+//zad2
 void wczyt(int *a, int *b)
 {
-    printf("podaj dwie liczby");
-scanf("%d %d",a,b);
+    printf("podaj pierwsza liczbe");
+    scanf("%d",a);
+    printf("podaj druga liczbe");
+    scanf("%d",b);
 }
 void operacje_proste(int a, int b, int *suma, int *roznica, int *iloczyn)
 {
-    *suma=*a+*b;
-    *roznica=*a-*b;
-    *iloczyn=*a*(*b);
+    *suma=a+b;
+    *roznica=a-b;
+    *iloczyn=a*b;
 }
-void operacje zaawansowane(int a, int b, int *iloraz, int *srednia)
+void operacje_zaawansowane(int a, int b, float *iloraz, float *srednia)
 {
+    *iloraz=(float)a/b;
+    *srednia=(float)(a+b)/2;
 
 }
+void wyniki(const int *suma, const int *roznica, const int *iloczyn, const float *iloraz, const float *srednia,int *a, int *b)
+{
+    printf("a wynosi: %d\n",*a);
+    printf("b wynosi: %d\n",*b);
+    printf("suma a i b wynosi: %d\n",*suma);
+    printf("roznica a i b wynosi: %d\n",*roznica);
+    printf("iloczyn a i b wynosi: %d\n",*iloczyn);
+    printf("iloraz a i b wynosi: %f\n",*iloraz);
+    printf("srednia a i b wynosi: %f\n",*srednia);
+}
+
+void ustaw(int tab[], int rozmiar)
+{
+    int i;
+    for(i=0;i<rozmiar;i++){
+        *tab[i]=(i+1)*10;
+    }
+}
+void druk(const int tab, const int rozmiar_tab, int *i, int *e)
+{
+    int j;
+    for(j=*i;j<=*e;j++){
+
+    }
+}
+
 int main()
 {
-   int a,b,c;
-   int suma, roznica, iloczyn;
-   float iloraz,srednia;
+   //zadanie1
+   //  int a,b,c;
   // wczytaj(&a,&b,&c);
   // sortuj(&a,&b,&c);
   // drukuj(&a,&b,&c);
-    return 0;
+
+  //zadanie2
+ /* int a,b;
+  int suma, roznica, iloczyn;
+   float iloraz,srednia;
+   wczyt(&a,&b);
+   operacje_proste(a,b,&suma,&roznica,&iloczyn);
+   operacje_zaawansowane(a,b,&iloraz,&srednia);
+   wyniki(&suma,&roznica,&iloczyn,&iloraz,&srednia,&a,&b);
+   */
+
+   //zadanie 3
+   int start,ile;
+   int tab[100];
+   ustaw(tab,100);
+   printf("podaj cyfre indeksu od ktorego drukujemy");
+   scanf("%d",&start);
+   printf("podaj ile elementow drukujemy");
+   scanf("%d",&ile);
+   druk(tab,rozmiar,start,ile);
+   return 0;
 }
